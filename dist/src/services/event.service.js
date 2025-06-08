3070800b40ff1c48b2242f4748a9891b95538082
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-06-08 12:27:17 +0700

    fix add event api

--- dist/src/services/event.service.js
+++ dist/src/services/event.service.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateEventData = void 0;
 const validateEventData = (data) => {
-    const { title, description, start_date, end_date, is_active } = data;
-    if (!title || !description || !start_date || !end_date || !is_active) {
+    const { title, description, start_time, end_time, is_active } = data;
+    if (!title || !description || !start_time || !end_time || !is_active) {
         throw new Error("Missing required fields!");
     }
 };
--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,7 +1,7 @@
 export const validateEventData = (data: any) => {
-  const { title, description, start_date, end_date, is_active } = data;
+  const { title, description, start_time, end_time, is_active } = data;
 
-  if (!title || !description || !start_date || !end_date || !is_active) {
+  if (!title || !description || !start_time || !end_time || !is_active) {
     throw new Error("Missing required fields!");
   }
 };
