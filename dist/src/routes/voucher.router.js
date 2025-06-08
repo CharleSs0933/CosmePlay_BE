fa6d133e35af032d07013694a3a0fa8c1a14c476
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-06-08 14:48:25 +0700

    Fix get voucher route

--- dist/src/routes/voucher.router.js
+++ dist/src/routes/voucher.router.js
@@ -7,6 +7,6 @@ const express_1 = __importDefault(require("express"));
 const isAuthenticated_1 = require("../packages/middleware/isAuthenticated");
 const voucher_controller_1 = require("../controller/voucher.controller");
 const router = express_1.default.Router();
-router.post("/", isAuthenticated_1.isAuthenticated, voucher_controller_1.getVouchersByUser);
+router.get("/", isAuthenticated_1.isAuthenticated, voucher_controller_1.getVouchersByUser);
 router.get("/all", isAuthenticated_1.isAuthenticated, (0, isAuthenticated_1.allowedRoles)(["admin", "staff"]), voucher_controller_1.getAllVouchers);
 exports.default = router;
--- src/routes/voucher.router.ts
+++ src/routes/voucher.router.ts
@@ -12,7 +12,7 @@ import {
 
 const router = express.Router();
 
-router.post("/", isAuthenticated, getVouchersByUser);
+router.get("/", isAuthenticated, getVouchersByUser);
 router.get(
   "/all",
   isAuthenticated,
